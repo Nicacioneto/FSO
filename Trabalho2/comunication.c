fabd12787f4f2919ec9f4fec24852773a7934ea7
lucaaas <lucasoliveira2306@gmail.com>
2017-04-19 21:20:47 -0300

    Added limit time of execution

--- Trabalho2/comunication.c
+++ Trabalho2/comunication.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include "writers.h"
 #include "childs.h"
 
-
+void finish_program(int signal) {
+  printf("finishing...\n");
+  exit(0);
+}
 
 int main(){
   int active_process_pipe[2];
@@ -41,18 +46,41 @@ int main(){
 
   /* Parent process */
   else {
+
+    signal(SIGALRM, finish_program);
+    alarm(10);
+
     close (active_process_pipe[1]);
     close (lazy_child_pipe[1]);
 
-    FILE* pipe_active_child;
-    pipe_active_child = fdopen (active_process_pipe[0], "r");
+    while(1){
 
-    FILE* pipe_lazy_child;
-    pipe_lazy_child = fdopen (lazy_child_pipe[0], "r");
+      fd_set fds;
+      int maxfd;
 
-    get_message_pipe_and_write_file (pipe_lazy_child);
+      FD_ZERO(&fds); // Clear FD set for select
+      FD_SET(active_process_pipe[0], &fds);
+      FD_SET(lazy_child_pipe[0], &fds);
 
-    close(active_process_pipe[0]);
+      maxfd = active_process_pipe[0] > lazy_child_pipe[0] ? active_process_pipe[0] : lazy_child_pipe[0];
+
+      select(maxfd + 1, &fds, NULL, NULL, NULL);
+
+      if (FD_ISSET(active_process_pipe[0], &fds)) {
+       FILE* pipe_active_child;
+       pipe_active_child = fdopen (active_process_pipe[0], "r");
+       get_message_pipe_and_write_file (pipe_active_child);
+       close(active_process_pipe[0]);
+      }
+
+      if (FD_ISSET(lazy_child_pipe[0], &fds)) {
+        FILE* pipe_lazy_child;
+        pipe_lazy_child = fdopen (lazy_child_pipe[0], "r");
+        get_message_pipe_and_write_file (pipe_lazy_child);
+        close(lazy_child_pipe[0]);
+      }
+
+    }
   }
 
   return 0;
