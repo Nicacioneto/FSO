0cfadc31009e5818ec286cb2de0c801c00abbc09
Nicacio Arruda <=>
2017-06-23 10:53:02 -0300

    Adding first files for trabalho 5

---
+++ trabalho05/buscador.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]){
+
+	int argumentos = atoi(argv[3]);
+
+	printf("Resultado do buscador -- '%s' na pasta %s\n", argv[2], argv[1]);
+	return(0);
+}
