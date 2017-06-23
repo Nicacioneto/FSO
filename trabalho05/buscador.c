6a11d39792b786b4ce98705c5035c88e5762b8de
Nicacio Arruda <=>
2017-06-23 12:03:33 -0300

    Adding dir_list function

--- trabalho05/buscador.c
+++ trabalho05/buscador.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
 
 
+
+void dir_list(char* dir_path){
+
+	DIR *dir;
+	struct dirent *entry;
+
+	dir = opendir(dir_path);
+
+	if(dir){
+		while((entry = readdir(dir)) != NULL){
+			printf("Passou");
+		}
+	}
+
+	closedir(dir);
+}
 int main(int argc, char *argv[]){
 
-	int argumentos = atoi(argv[3]);
+	int args = atoi(argv[3]);
 
 	printf("Resultado do buscador -- '%s' na pasta %s\n", argv[2], argv[1]);
+	dir_list(argv[1]);
+
 	return(0);
 }
